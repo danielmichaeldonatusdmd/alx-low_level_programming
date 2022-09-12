91fb4ea3ed74c834e7822a22ec6f3dfeccee9a13
daniel <danielmichaeldonatusdmd>
2022-09-11 23:11:23 -0700

    another C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print using printf not puts
+ *
+ * Return:(0)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
