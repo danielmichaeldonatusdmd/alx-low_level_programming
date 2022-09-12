8bce145389c5d9e4b5b6151931a9b4af152aeeca
daniel <danielmichaeldonatusdmd>
2022-09-11 21:42:20 -0700

    my first C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,13 @@
 #include  <stdio.h>
 
-
-
 /**
-
- * main - Entry point
-
- * Description: C program that prints a function followed by a new line
-
- * Return: 0 if successful
-
+ * main - entry point
+ * Description: print a function followed by a new line
+ * Return: 0 is succesful
  */
 
-
-
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-
 }
