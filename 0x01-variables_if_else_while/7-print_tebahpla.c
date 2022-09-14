7280ea898ce6b21db8591f7fea0777cb13ac700a
daniel <danielmichaeldonatusdmd>
2022-09-14 16:12:14 -0700

    print reversely

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,4 +3,18 @@
 /**
  * main -  prints the lowercase alphabet in reverse followed by a \n
  *
- *
+ * Return: 0 Always (success)
+ */
+
+int main(void) {
+
+
+	char ALPHA;
+
+	for (ALPHA = 'z'; ALPHA >= 'a'; ALPHA--)
+	{
+		putchar(ALPHA);
+	}
+	putchar('\n');
+	return (0);
+}
