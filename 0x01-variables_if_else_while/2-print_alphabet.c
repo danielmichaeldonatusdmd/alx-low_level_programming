f9056851f82c95fe8a837faac03878df83348842
daniel <danielmichaeldonatusdmd>
2022-09-14 07:56:59 -0700

    print alphaet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: 0 return (success)
+ */
+
+int main(void)
+{
+	int ch;
+
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return(0);
+}
