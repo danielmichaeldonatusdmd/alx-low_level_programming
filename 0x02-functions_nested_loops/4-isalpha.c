6b400e6c5395e5a1f749d6f48a20d684461f7431
daniel <danielmichaeldonatusdmd>
2022-09-17 01:42:55 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,9 @@
 #include "main.h"
  /**
   * _isalpha - Write a function that checks for alphabetic character.
-  * @APH: execute alhabet
+  * @APH: character execute
   * Return: 1 if c is a letter, 0 otherwise
   */
-
 int _isalpha(int APH)
 {
 	return ((APH >= 'a' && APH <= 'z') || (APH >= 'A' && APH <= 'Z'));
