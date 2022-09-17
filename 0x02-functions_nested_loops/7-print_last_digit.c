1e36d9d5cb28e0f29ee3db943e1cd6231b3947a8
daniel <danielmichaeldonatusdmd>
2022-09-17 04:52:44 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,29 +1,26 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints the last digit of a number
+ * print_last_digit - prints the last digit of the given vakue
  *
- * @n: the int to extract the last digit from
- *
- * Return: value of the last digit
- *
+ * @MD: execute digit
+ * Return: 0 Always (succesful)
  */
 
-int print_last_digit(int n)
+int print_last_digit(int MD)
 {
-	int a;
+	int remain = MD % 10;
 
-	if (n < 0)
-		n = -n;
-
-		a = n % 10;
-
-		if (a < 0)
-
-			a = -a;
-
-			_putchar(a + '0');
-
-	return (a);
-
-}
+	if (G < 0)
+	{
+		G = -(G);
+		_putchar(G + '0');
+		return (G);
+	}
+	else
+	{
+		_putchar(G + '0');
+		return (G);
+	}
+	return (0);
+}	
