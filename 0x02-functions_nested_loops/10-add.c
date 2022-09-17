fdcdaabd4e0313cde07eb583348ac2b4e822b5a1
daniel <danielmichaeldonatusdmd>
2022-09-17 10:57:47 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @ME: first input
+ * @YOU: second input
+ * Return: ME + YOU
+ */
+
+int add(int ME, int YOU)
+{
+	int _ME/YOU_;
+	{
+		_ME/YOU_ = (ME + YOU)
+	}
+	return (_ME/YOU);
+}
