ab50d8fadb9a92e705f13fc864319d68f51ddcba
daniel <danielmichaeldonatusdmd>
2022-09-17 14:21:43 -0700

    times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -19,24 +19,24 @@ void print_times_table(int n)
 				if (Y == 0)
 				{
 					_putchar(Z + '0');
-				}
-				else if (Z < 10 && Y != 0)
+
+				} else if (Z < 10 && Y != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(Z + '0');
-				}
-				else if (Z >= 10 && Z < 100)
+
+				} else if (Z >= 10 && Z < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((Z / 10) + '0');
 					_putchar((Z % 10) + '0');
-				}
-				else if (Z >= 100)
+
+				} else if (Z >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
