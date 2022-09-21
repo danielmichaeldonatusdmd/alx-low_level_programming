e36ca445cbd30233a51875909e5089ec8bcff614
daniel <danielmichaeldonatusdmd>
2022-09-21 16:52:58 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - that generates random valid passwords
+ * for the program 101-crackme.
+ *
+ * Return: 0
+ *
+ */
+
+
+int main(void)
+{
+	int m;
+	char p;
+
+	srand(time(NULL));
+	while (m <= 2645)
+	{
+		p = rand() % 128;
+		m += p;
+		putchar(c);
+	}
+	putchar(2772 - m);
+	return (0);
+}
