008ae304a5a61a99c00b505fae00759f2cda583c
daniel <danielmichaeldonatusdmd>
2022-09-21 18:29:34 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  main - this main file is a test for a postitive_or_negative() function
+ *
+ * Return: 0 result
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
