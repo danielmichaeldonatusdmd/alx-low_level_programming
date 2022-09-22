3091c6ca7f1f737befa228a71c62feacef90ab39
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-09-22 16:10:31 -0700

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13
+ *
+ * @s: sprint
+ *
+ * Return: pointer
+ */
+
+char *rot13(char *s)
+{
+	int m = 0, i;
+	char aZ[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char mix[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + m) != '\0')
+	{
+
+		for (i = 0; i < 52; i++)
+		{
+			if (*(s + aZ) == aZ[i])
+			{
+				*(s + m) = mix[i];
+				break;
+			}
+		}
+		m++;
+	}
+
+	return (s);
+
+}}
