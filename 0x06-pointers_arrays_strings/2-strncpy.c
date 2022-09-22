6abe110eddea7e0a1aa019b47b89e875fe6424ec
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-09-22 14:12:45 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strncpy - unction that concatenates two strings.
+ * @dest: the pointer to the destination string
+ * @src: the pointer to the source string
+ * @n: number of bytes from src to be copied
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
