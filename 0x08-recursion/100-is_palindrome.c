6fa5f00fc25d717cfe9d196e8c717d6871b435c3
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-10-04 14:03:25 -0700

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+int _palindrome(char *s, int p);
+
+/**
+ * _strlen_recursion - size
+ *	@s: pointer to string parameter
+ * Return: recursion
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * _paliindrome - palindrome
+ *	@s: pointer to string
+ *	@p: position
+ * Return: boolean
+ */
+
+int pali(char *s, int p)
+{
+	if (p < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + p))
+	{
+		return (pali(s + 1, p - 2));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ *	@s: pointer to string
+ * Return: recursion
+ */
+
+int is_palindrome(char *s)
+{
+	int len = _strlen_recursion(s);
+
+	return (pali(s, len - 1));
+}
