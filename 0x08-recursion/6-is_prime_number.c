5f78d43498aaa0e48155cb9698a8430f3c103eab
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-10-04 12:54:41 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,35 +1,36 @@
 #include "main.h"
 
+int is_prime(int n, int zz);
 /**
  * is_prime_number - returns the 1 if n is prime
- * @n: number to be checked
+ *	@n: number to be checked
  * Return: 1 if n is prime, 0 otherwise
  */
 
 
 int is_prime_number(int n)
 {
-	int start = n / 2;
+	int zz = n / 2;
 
 	if (n <= 1)
 		return (0);
-	return (is_prime(n, start));
+	return (is_prime(n, zz));
 }
 
 
 /**
  * is_prime - returns the 1 if n is prime
- * @n: number to be checked
- * @start: number to start checking from
+ *	@n: number to be checked
+ *	@zz: number to start checking from
  * Return: 1 if n is prime, 0 otherwise
  */
 
 
-int is_prime(int n, int start)
+int is_prime(int n, int zz)
 {
-	if (start <= 1)
+	if (zz <= 1)
 		return (1);
-	else if (n % start == 0)
+	else if (n % zz == 0)
 		return (0);
-	return (is_prime(n, start - 1));
+	return (is_prime(n, zz - 1));
 }
