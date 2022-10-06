c844eb21cd6b2b215321973f1e5b824e9e706348
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-10-05 18:41:30 -0700

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds numbers. checks if number
+ *	@argc: number of arguments
+ *	@argv: the vector that hold the arguments
+ * Return: integer
+ */
+
+int main(int argc, char **argv[])
+{
+
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+
+	{
+
+		for (j = 0; argv[i][j] != '\0'; j++)
+
+	{
+
+		if (!isdigit(argv[i][j]))
+
+		{
+
+			printf("Error\n");
+
+			return (1);
+
+		}
+
+	}
+
+	sum += atoi(argv[i]);
+
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
