afb91a9c40304b1077ff558f269b057a74503620
danielmichaeldonatusdmd <danieldonatusmichaelddm@gmain.com>
2022-10-05 17:23:27 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 always
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
